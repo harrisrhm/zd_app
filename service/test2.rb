2353de7790552d0bbe8422503d3997f79885902c
Harris Rahim <harris.rhm@gmail.com>
2021-05-13 16:08:19 +1000

    api: tested with zendesk api and return ticket objects  as expected

--- service/test2.rb
+++ service/test2.rb
@@ -1,26 +1,66 @@
 require 'json'
-require 'zendesk_api'
+require 'open-uri'
+require "httparty"
 
-class ZendeskService
-    def initialize
-        @client = ZendeskAPI::Client.new do |config|
-        config.url = "https://harrisrhm.zendesk.com/api/v2"
-        config.username = "harris.rhm@gmail.com"
-        config.token = "6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm"
-        # Optional:
-        config.retry = true
-        require 'logger'
-        config.logger = Logger.new(STDOUT)
-    end
-  end
+# HTTParty.get("http://dataineed.com/data.json")
+# JSON.parse(response.body)
 
-  def get_client
-    @client
-  end
 
-end
+# auth = {
+#   Authorization: Basic 6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm,
+# }
+# options = { basic_auth: auth }
+# result = HTTParty.get("https://harrisrhm.zendesk.com/api/v2/tickets.json", options)
+# p result
 
-zendesk_client = ZendeskService.new().get_client
-tickets = zendesk_client.tickets
-all = tickets.fetch!
-puts all
+Authorization: Bearer '6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm'
+
+
+# uri = URI("https://harrisrhm.zendesk.com/api/v2/tickets.json")
+# auth = {
+#     Authorization: Bearer '6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm'
+# }
+
+# http = Net::HTTP.new(uri.host, uri.port)
+# response = http.get(uri, auth)
+# p response
+
+
+# require 'zendesk_api'
+
+# class ZendeskService
+#     def initialize
+#         @client = ZendeskAPI::Client.new do |config|
+#         config.url = "https://harrisrhm.zendesk.com/api/v2"
+#         config.username = "harris.rhm@gmail.com"
+#         config.token = "6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm"
+#         # Optional:
+#         config.retry = true
+#         require 'logger'
+#         config.logger = Logger.new(STDOUT)
+#     end
+#   end
+
+#   def get_client
+#     @client
+#   end
+
+# end
+
+# new = ZendeskService.new
+# main = new.get_client
+# one = main.tickets.fetch!
+
+# description = one.select { |ticket| ticket["description"] }[0] ||= {}
+# hash = []
+# hash.push({ "description" => description })
+# puts hash
+# zendesk_client = ZendeskService.new().get_client
+# tickets = zendesk_client.tickets.page(1).per_page(25)
+# next_page = tickets.next
+# all = tickets.fetch!
+
+# zendesk_client.tickets.all! do |resource|
+#    p resource["id"]
+#    p resource["requester"]
+# end
