0ee8f5eb3fed58206b089e72e1424410cb976efe
Harris Rahim <harris.rhm@gmail.com>
2021-05-14 10:47:46 +1000

    model: created a ticket model with attributes to initialize

---
+++ lib/ticket.rb
@@ -0,0 +1,14 @@
+class Ticket
+    attr_reader :ticket_id, :requester, :subject, :description, :created_date, :created_time, :status
+  
+    def initialize(attributes = {})
+      @ticket_id = attributes[:ticket_id]
+      @requester = attributes[:requester]
+      @subject = attributes[:subject]
+      @description = attributes[:description]
+      @created_date = attributes[:created_date]
+      @created_time = attributes[:created_time]
+      @status = attributes[:status]
+    end
+
+end
--- service/test2.rb
+++ service/test2.rb
@@ -1,35 +1,39 @@
-require 'json'
-require 'zendesk_api'
+require "json"
+require "zendesk_api"
+require_relative "../lib/ticket"
+
 
 class ZendeskService
     def initialize
-        @client = ZendeskAPI::Client.new do |config|
-        config.url = "https://harrisrhm.zendesk.com/api/v2"
-        config.username = "harris.rhm@gmail.com"
-        config.token = "6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm"
-        # Optional:
-        config.retry = true
-        require 'logger'
-        config.logger = Logger.new(STDOUT)
+      @client = ZendeskAPI::Client.new do |config|
+      config.url = "https://harrisrhm.zendesk.com/api/v2"
+      config.username = "harris.rhm@gmail.com"
+      config.token = "6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm"
+      # Optional:
+      config.retry = true
+      require 'logger'
+      config.logger = Logger.new(STDOUT)
+      end
     end
-  end
 
-  def get_client
-    @client
-  end
-
+    def get_client
+      results = []
+      tickets = @client.tickets.to_a
+      tickets.each do |ticket|
+        ticket_id = ticket["id"]
+        requester = ticket["requester_id"]
+        subject = ticket["subject"]
+        description = ticket["description"]
+        created_date = ticket["created_at"].strftime("%d %B %Y")
+        created_time = ticket["created_at"].strftime("%H:%M%p")
+        status = ticket["status"]
+        results << Ticket.new(ticket_id: ticket_id, requester: requester, subject: subject, description: description, created_date: created_date, created_time: created_time, status: status)
+      end
+      return results
+    end
 end
 
-zendesk_client = ZendeskService.new().get_client
-# tickets = zendesk_client.tickets.page(1).per_page(25)
-# next_page = tickets.next
-tickets = zendesk_client.tickets.to_a
-tickets.each do |ticket|
-  ticket_id = ticket["id"]
-  requester = ticket["requester_id"]
-  subject = ticket["subject"]
-  created_date = ticket["created_at"].strftime("%d %B %Y")
-  created_time = ticket["created_at"].strftime("%H:%M%p")
-  status = ticket["status"]
-  # puts "Ticket_id: #{ticket["id"]}, Requester: #{ticket["requester_id"]}, Subject: #{ticket["subject"]}, Created: #{ticket["created_at"].strftime("%d %B %Y")} at #{ticket["created_at"].strftime("%H:%M%p")}, Status: #{ticket["status"]}"
-end
+
+new = ZendeskService.new
+main = new.get_client
+puts main
