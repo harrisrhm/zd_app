dcd6fcc739baa7f372229067f8b4124d5c23d63c
Harris Rahim <harris.rhm@gmail.com>
2021-05-16 12:26:37 +1000

    zendeskservice: deleted test.rb file as it is not needed

--- service/test.rb
+++
@@ -1,6 +0,0 @@
-require "httpclient"
-require "json"
-require "open-uri"
-
-result = JSON.parse(open("https://harrisrhm.zendesk.com/api/v2/tickets.json", :http_basic_authentication=>['harris.rhm@gmail.com/token', '6GB65xsrJb4V7HevT3xlbZUyquFNZtUn4POBvenm']).read)
-puts result
